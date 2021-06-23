65138e528fc562d83139310fc73216f400b927c5
jihedlaribi <jihed.laribi13@gmail.com>
2021-06-23 17:07:44 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import pic2 from "./picsrc.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="jihed">
+      <iframe className="jiji"
+      
+       
+        src="https://www.youtube.com/embed/NN1RWBgZ62w"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe></div>
+      <div className="img" style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+ 
+
+        <br />
+
+        <img src={pic2} alt=""  />
+
+        <br />
+
+        <img
+          src="/picpublic.jpg"
+          alt="jihed"
+          
+        />
+      </div>
+ 
     </div>
   );
 }
